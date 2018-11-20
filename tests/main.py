e18e83d461d157f50a9ce50f488380da5a84b16d
Enrique Y. German Totosaus <yishaq95@gmail.com>
2018-11-20 00:54:09 -0600

    Support for new featurs

--- tests/main.py
+++ tests/main.py
@@ -1,39 +1,68 @@
 import logging
-from interpreter.Reader import Reader
-from interpreter.Lexer import Lexer
 from interpreter.Container import Container
+from interpreter.Reader import Reader
+from interpreter.SymbolsTable import SymbolsTable
+from interpreter.automata.Automata import Automata
+from interpreter.Lexer import Lexer
+from interpreter.Sintaxer import Sintaxer
 
-test_path = "./tests/"
+test_path = "./config/"
 
 def setUp_Logger(log_level):
     # set up logging to file - see previous section for more details
     logging.basicConfig(level=log_level,
                         format='%(name)s:%(levelname)s: %(message)s')
 
-def main():
-    setUp_Logger(logging.DEBUG)
-    lexer = Lexer()
-# setUp_Automata==========================================
-    automata_descripter = test_path+"conf/automata_description/1.txt"
-    reader = Reader( automata_descripter )
-    reader.loadData()
-    lexer.setAutomata( reader.getData() )
-# setUp_SymbolsTable==========================================
-    symbols_table_descripter = test_path+"conf/symbols_table/1.txt"
+def setUp_SymbolsTable():
+    symbols_table_descripter = test_path+"/symbols_table/PF.txt"
     reader = Reader( symbols_table_descripter )
     reader.loadData()
-    lexer.setSymbolsTable( reader.getData() )
-# run_Lexer=======================================
-    source = test_path+"source/strings.txt"
+    symbols_table = SymbolsTable()
+    symbols_table.loadFromDict( reader.getData() )
+    symbols_table.addFunction( 'ESCRIBE', ['nada'] ) # 'nada' because it should print anything
+    symbols_table.addFunction( 'LEER', ['apuntador'] )
+    symbols_table.addID('caracter', 'putoJuanjo', 10)
+    symbols_table.addID('caracter', 'putoJuanjo', 10)
+
+    print(symbols_table)
+    return symbols_table
+
+def setUp_Automata():
+    automata_descripter = test_path+"/automata/PF.txt"
+    reader = Reader( automata_descripter )
+    reader.loadData()
+    automata = Automata()
+    automata.loadFromDict( reader.getData() )
+
+    print(automata)
+    return automata
+
+def setUp_Lexer(symbols_table, automata, text):
+    lexer = Lexer()
+    lexer.setSymbolsTable( symbols_table )
+    lexer.setAutomata( automata )
+    lexer.setSource( Container(text) )
+    return lexer
+
+def getSourceText():
+    source = "./tests/input/PF_pares.txt"
     text = None
     with open( source ) as fp:
         text = fp.read()
-    lexer.setSource( Container(text) )
+    return text
+
+def main():
+    setUp_Logger(logging.DEBUG)
+
+    symbols_table = setUp_SymbolsTable()
+    automata = setUp_Automata()
+    text = getSourceText()
+    lexer = setUp_Lexer(symbols_table, automata, text)
+
+# run_Lexer=======================================
     while( True ):
         token = lexer.nextToken()
-        print( "\n>>>Token:"+ str(token) )
-        input(">>")
-        print("\n")
+        print( str(token) + "\n" )
 
 if __name__ == '__main__':
     main()
